1493fe8f6f9f430cb4b5e1e54d4e81a69fce704d
SanzharShadybekov <shadubekovs777@gmail.com>
2023-04-11 20:47:07 +0600

    added git instructions

--- gitlessons/intro.py
+++ gitlessons/intro.py
@@ -17,10 +17,13 @@
 
 # 5. git push <origin> <main> -  команда отправки всех ваших изменений на удаленный репо
 
+# -----------------------
 
-
-
-
+# 1*) git init
+#         2) git add .
+#         3) git commit -m ''
+# 4*) git remote add origin <url>
+#         5) git push origin main
 
 
 
